5f0e5dbf1afc600a42bc16186d91aaa70b6f0ab8
Gwan-gyeong Mun <gwan-gyeong.mun@intel.com>
2024-07-10 15:32:00 +0300

    configuration for mesa

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 set -x
 rm -rf ./build
-meson build
+#meson build -Dgallium-drivers=iris -Dvulkan-drivers=intel -Dvulkan-drivers=intel
+#meson build -Dgallium-drivers=iris -Dvulkan-drivers=intel -Dvulkan-drivers=intel -Dbuildtype=debug -Dgallium-rusticl=true -Dllvm=enabled 
+RUSTC=clippy-driver meson build -Dgallium-drivers=iris -Dvulkan-drivers=intel -Dvulkan-drivers=intel -Dbuildtype=debug -Dgallium-rusticl=true -Dllvm=enabled -Drust_std=202
 ninja -C build
